0c5bf1ada3ae6c28df6abdddcc2923c30090095b
riri97-igm <ingyinlay.97@gmail.com>
2024-08-02 13:18:51 +0800

    Init

---
+++ src/test/java/org/example/lioneatsjava/LioneatsJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.lioneatsjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LioneatsJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
